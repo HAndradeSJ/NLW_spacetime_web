af6aa8795ef6135246c88944832db5fd212eec57
HAndradeSJ <henriqueandradesj@outlook.com>
2023-05-18 19:03:28 -0300

    version inicial

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins:[
+        require('pretttier-plugin-tailwindcss')
+    ],
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+// importação
+export default function Home() {
+  
+  return <div className="h-screen bg-zinc-900 text-zinc-50 font-alt ">oi</div>
+}
