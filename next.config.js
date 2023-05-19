a1893952e63197f7bfbb9a5d2507974544d61e89
HAndradeSJ <henriqueandradesj@outlook.com>
2023-05-19 17:37:41 -0300

    version webs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    //configurando a função images pq o next nn reconehece sem ser arquivo
+    images:{
+        domains:[
+            'avatars.githubusercontent.com',    
+        ]
+    }
+}
 
 module.exports = nextConfig
